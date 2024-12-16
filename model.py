0b9cf0306aa89de378a917ecf78434dbb7617542
valentin.dorseuil@gmail.com <valentin.dorseuil@gmail.com>
2024-12-16 01:42:18 +0100

    Naming mistake for the layer norms in the encoder

--- model.py
+++ model.py
@@ -3,26 +3,6 @@ import torch
 import torch.nn as nn
 
 
-class Embedding(nn.Module):
-    def __init__(self, batch_size, model_dim, max_length, n_embedding):
-        super().__init__()
-        self.max_length = max_length
-        self.batch_size = batch_size
-        self.model_dim = model_dim
-        self.n_embedding = n_embedding
-        self.embedding = torch.nn.Embedding(
-            num_embeddings=n_embedding, embedding_dim=model_dim
-        )
-        self.pos_encoding = PositionalEncoding(
-            batch_size=batch_size, model_dim=model_dim, max_length=max_length
-        )
-        pass
-
-    def forward(self, x):
-        x = self.embedding(x) + self.pos_encoding(x)
-        return x
-
-
 class PositionalEncoding(nn.Module):
     def __init__(self, batch_size, model_dim, max_length):
         super().__init__()
@@ -38,17 +18,28 @@ class PositionalEncoding(nn.Module):
             return np.cos(pos / 10000 ** (2 * i / self.model_dim))
 
     def compute(self):
-        Mat = torch.Tensor(
-            [
-                [self.SinPos(i, pos) for i in range(self.model_dim)]
-                for pos in range(self.max_length)
-            ]
-        )
+        Mat = torch.Tensor([[self.SinPos(i, pos) for i in range(self.model_dim)] for pos in range(self.max_length)])
         self.Mat = Mat.expand(self.batch_size, -1, -1)
 
     def forward(self, x):
         with torch.no_grad():
             return self.Mat
+
+
+class Embedding(nn.Module):
+    def __init__(self, batch_size, model_dim, max_length, n_embedding):
+        super().__init__()
+        self.max_length = max_length
+        self.batch_size = batch_size
+        self.model_dim = model_dim
+        self.n_embedding = n_embedding
+        self.embedding = torch.nn.Embedding(num_embeddings=n_embedding, embedding_dim=model_dim)
+        self.pos_encoding = PositionalEncoding(batch_size=batch_size, model_dim=model_dim, max_length=max_length)
+        pass
+
+    def forward(self, x):
+        x = self.embedding(x) + self.pos_encoding(x)
+        return x
 
 
 class SingleHeadAttention(nn.Module):
@@ -63,12 +54,8 @@ class SingleHeadAttention(nn.Module):
         self.mask = mask
 
     def forward(self, x: torch.Tensor, x_encoder: torch.Tensor = None):
-        if x_encoder is not None:
-            Kx = self.K(x_encoder)
-            Vx = self.V(x_encoder)
-        else:
-            Kx = self.K(x)
-            Vx = self.V(x)
+        Kx = self.K(x_encoder) if x_encoder is not None else self.K(x)
+        Vx = self.V(x_encoder) if x_encoder is not None else self.V(x)
         Qx = self.Q(x)
         QK = torch.matmul(Qx, Kx.transpose(-2, -1)) / np.sqrt(self.dk)
         if self.mask is not None:
@@ -80,19 +67,13 @@ class SingleHeadAttention(nn.Module):
 class MultiHeadAttention(nn.Module):
     def __init__(self, num_heads: int, dk: int, dv: int, model_dim: int, mask=None):
         super().__init__()
-        assert (
-            num_heads * dv == model_dim
-        ), "num_heads * dv should be equal to the model dim"
-        self.attention_heads = nn.ModuleList(
-            [SingleHeadAttention(dk, dv, model_dim, mask) for _ in range(num_heads)]
-        )
+        assert num_heads * dv == model_dim, "num_heads * dv should be equal to the model dim"
+        self.attention_heads = nn.ModuleList([SingleHeadAttention(dk, dv, model_dim, mask) for _ in range(num_heads)])
         self.WO = nn.Linear(in_features=num_heads * dv, out_features=model_dim)
         self.mask = mask
 
     def forward(self, x: torch.Tensor, x_encoder: torch.Tensor = None):
-        outputs = []
-        for head in self.attention_heads:
-            outputs.append(head(x, x_encoder))
+        outputs = [head(x, x_encoder) for head in self.attention_heads]
         x = torch.cat(outputs, dim=-1)
         x = self.WO(x)
         return x
@@ -101,10 +82,9 @@ class MultiHeadAttention(nn.Module):
 class EncoderBlock(nn.Module):
     def __init__(self, num_heads, dk, dv, model_dim):
         super().__init__()
-        self.attention = MultiHeadAttention(
-            num_heads=num_heads, dk=dk, dv=dv, model_dim=model_dim
-        )
-        self.layerNorm = nn.LayerNorm(normalized_shape=model_dim)
+        self.attention = MultiHeadAttention(num_heads=num_heads, dk=dk, dv=dv, model_dim=model_dim)
+        self.layerNorm1 = nn.LayerNorm(normalized_shape=model_dim)
+        self.layerNorm2 = nn.LayerNorm(normalized_shape=model_dim)
         self.ff = nn.Sequential(
             nn.Linear(in_features=model_dim, out_features=2048),
             nn.ReLU(),
@@ -113,18 +93,16 @@ class EncoderBlock(nn.Module):
 
     def forward(self, x):
         attention = self.attention(x)
-        x = self.layerNorm(x + attention)
+        x = self.layerNorm1(x + attention)
         feedforward = self.ff(x)
-        x = self.layerNorm(x + feedforward)
+        x = self.layerNorm2(x + feedforward)
         return x
 
 
 class Encoder(nn.Module):
     def __init__(self, num_heads, dk, dv, model_dim, num_encoders):
         super().__init__()
-        self.encoders_list = [
-            EncoderBlock(num_heads, dk, dv, model_dim) for _ in range(num_encoders)
-        ]
+        self.encoders_list = [EncoderBlock(num_heads, dk, dv, model_dim) for _ in range(num_encoders)]
         self.encoders = nn.Sequential(*self.encoders_list)
 
     def forward(self, x):
@@ -141,10 +119,11 @@ class DecoderBlock(nn.Module):
         self.masked_attention = MultiHeadAttention(
             num_heads=num_heads, dk=dk, dv=dv, model_dim=model_dim, mask=self.mask
         )
-        self.mixed_attention = MultiHeadAttention(
-            num_heads=num_heads, dk=dk, dv=dv, model_dim=model_dim
-        )
-        self.layerNorm = nn.LayerNorm(normalized_shape=model_dim)
+        self.mixed_attention = MultiHeadAttention(num_heads=num_heads, dk=dk, dv=dv, model_dim=model_dim)
+        self.layerNorm1 = nn.LayerNorm(normalized_shape=model_dim)
+        self.layerNorm2 = nn.LayerNorm(normalized_shape=model_dim)
+        self.layerNorm3 = nn.LayerNorm(normalized_shape=model_dim)
+
         self.ff = nn.Sequential(
             nn.Linear(in_features=model_dim, out_features=2048),
             nn.ReLU(),
@@ -153,11 +132,11 @@ class DecoderBlock(nn.Module):
 
     def forward(self, x, x_encoder):
         attention = self.masked_attention(x)
-        x = self.layerNorm(x + attention)
+        x = self.layerNorm1(x + attention)
         attention = self.mixed_attention(x, x_encoder)
-        x = self.layerNorm(x + attention)
+        x = self.layerNorm2(x + attention)
         feedforward = self.ff(x)
-        x = self.layerNorm(x + feedforward)
+        x = self.layerNorm3(x + feedforward)
         return x
 
 
@@ -175,10 +154,7 @@ class CustomSequential(nn.Module):
 class Decoder(nn.Module):
     def __init__(self, num_heads, dk, dv, model_dim, max_length, num_decoders):
         super().__init__()
-        decoders_list = [
-            DecoderBlock(num_heads, dk, dv, model_dim, max_length)
-            for _ in range(num_decoders)
-        ]
+        decoders_list = [DecoderBlock(num_heads, dk, dv, model_dim, max_length) for _ in range(num_decoders)]
         self.decoders = CustomSequential(*decoders_list)
 
     def forward(self, x, x_encoder):
@@ -216,7 +192,6 @@ class Transformer(nn.Module):
             num_decoders=num_decoders,
             max_length=max_length,
         )
-        self.softmax = nn.Softmax(dim=-1)
         self.linear = nn.Linear(in_features=model_dim, out_features=num_out)
         self.embedding = Embedding(
             batch_size=batch_size,
@@ -230,5 +205,4 @@ class Transformer(nn.Module):
         x_encoder = self.encoder(x)
         x = self.decoder(x, x_encoder)
         x = self.linear(x)
-        x = self.softmax(x)
         return x
