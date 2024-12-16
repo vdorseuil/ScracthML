0750e9111cfe2f92f8ea747c12bea220f5b40861
valentin.dorseuil@gmail.com <valentin.dorseuil@gmail.com>
2024-12-16 20:26:39 +0100

    bpe_tokenizer, data.txt, test.ipynb, train.py

--- transformers/train.py
+++ transformers/train.py
@@ -20,23 +20,38 @@ dropout = 0.1
 num_encoders = 6
 num_decoders = 6
 
+
+def init_transformer():
+    model = Transformer(
+        batch_size=batch_size,
+        model_dim=model_dim,
+        max_length=max_length,
+        vocab_size=vocab_size,
+        num_out=num_out,
+        num_heads=num_heads,
+        dv=dv,
+        dk=dk,
+        d_ff=d_ff,
+        dropout=dropout,
+        num_encoders=num_encoders,
+        num_decoders=num_decoders,
+    )
+    for p in model.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform_(p)
+    return model
+
+
+
+def inference(model, input, max_gen_length):
+    x = input
+    for i in range(max_gen_length):
+        out = torch.softmax(model(x), dim=-1)
+        next_token = torch.max(out, dim=-1)
+
+
 x = torch.randint(0, vocab_size, (batch_size, max_length))
-
-MyTransformer = Transformer(
-    batch_size=batch_size,
-    model_dim=model_dim,
-    max_length=max_length,
-    vocab_size=vocab_size,
-    num_out=num_out,
-    num_heads=num_heads,
-    dv=dv,
-    dk=dk,
-    d_ff=d_ff,
-    dropout=dropout,
-    num_encoders=num_encoders,
-    num_decoders=num_decoders,
-)
-
+MyTransformer = init_transformer()
 
 
 if __name__ == "__main__":
