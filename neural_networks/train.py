97968881d7509f095028a58788b015f0c3c2148e
valentin.dorseuil@gmail.com <valentin.dorseuil@gmail.com>
2024-12-30 02:44:48 +0100

    change train.py for neural networks (csv locations)

--- neural_networks/train.py
+++ neural_networks/train.py
@@ -2,15 +2,14 @@ import numpy as np
 import pandas as pd
 from matplotlib  import pyplot as plt
 
-data_train = pd.read_csv("/Users/valentin/Desktop/PYTHON/neural_network/mnist_train.csv")
-data_test = pd.read_csv("/Users/valentin/Desktop/PYTHON/neural_network/mnist_test.csv")
+from model import Network
+
+data_train = pd.read_csv("mnist_train.csv")
+data_test = pd.read_csv("mnist_test.csv")
 
 data_train = np.array(data_train)
 data_test = np.array(data_test)
-# for k in range(3):
-#     plt.imshow(data_train[k][1:].reshape((28, 28)), cmap = 'gray')
-#     plt.title('Value = {}'.format(data_train[k][0]))
-#     plt.show()
+
 
 m, n = data_train.shape
 data_train = data_train.T
@@ -46,8 +45,7 @@ test_data = batch(X_test, Y_test)
 liste_layer = [784, 16, 16, 10]
 
 
-import network
 
-net = network.Network(liste_layer)
-
-net.train_mini_batch(train_data[0:6000], 100, 100, 0.1, test_data)
+if __name__ == "__main__":
+    net = Network(liste_layer)
+    net.train_mini_batch(train_data[0:6000], 100, 100, 0.1, test_data)
